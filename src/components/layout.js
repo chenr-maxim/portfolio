95aa04a7f197079e95d0e4cfdf87d6ab6246f1d2
Maxim Chen <chenr.maxim@gmail.com>
2021-05-04 13:28:53 -0700

    comment out photoitems for the time being for production

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children }) {
           </Col>
         </Row>
         <Row>
-          <PhotoItems />
+          {/* <PhotoItems /> */}
           <Footer />
         </Row>
       </Container>
